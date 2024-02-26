010d241477717a7f0075e68a815cd728b09dc70f
JuliaMichaela <juliaperez@yandex.ru>
2024-02-26 23:53:39 +0300

    added onclick button Find out the cost

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ sayJS ();
 
 //Практическое здание 3
 const img = document.getElementById('img');
+const content = document.getElementById('content');
 
 function showNext(){
     img.src = './assets/images/ Faroe-Island.jpeg'
@@ -22,8 +23,10 @@ function showPrev(){
 
 function calculateTotalPrice(quantity = 2, price = 15000000) {
     const result = quantity * price;
-    return result.toLocaleString('ru-RU');
+    return content.textContent = `Стоимость покупки: ${result.toLocaleString('ru-RU')} рублей`
+
 }
+
 
 console.log (calculateTotalPrice())
 
